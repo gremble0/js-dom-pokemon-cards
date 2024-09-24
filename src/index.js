80cd2c00d944e79d7a7f5353e0ff75e72f6f95aa
gremble0 <hermanstornes@gmail.com>
2024-09-24 10:56:39 +0200

    Complete core

--- src/index.js
+++ src/index.js
@@ -1,6 +1,49 @@
+const cards = document.querySelector(".cards");
 
-console.log(data);
+function createH2(header) {
+  const h2 = document.createElement("h2");
+  h2.setAttribute("class", "card--title");
+  h2.innerHTML = header;
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+  return h2;
+}
+
+function createImg(src) {
+  const img = document.createElement("img");
+  img.setAttribute("class", "card--img");
+  img.setAttribute("src", src);
+  img.setAttribute("width", "256");
+
+  return img;
+}
+
+function createStat(stat) {
+  const statElement = document.createElement("li");
+  statElement.innerHTML = stat.stat.name.toUpperCase() + ": " + stat.base_stat;
+
+  return statElement;
+}
+
+function createStats(stats) {
+  const statsElement = document.createElement("ul");
+  statsElement.setAttribute("class", "card--text");
+
+  stats.forEach((stat) => statsElement.appendChild(createStat(stat)));
+
+  return statsElement;
+}
+
+function createPokemonElement(pokemon) {
+  const element = document.createElement("li");
+  element.setAttribute("class", "card");
+
+  element.appendChild(createH2(pokemon.name));
+  element.appendChild(createImg(pokemon.sprites.front_default));
+  element.appendChild(createStats(pokemon.stats));
+
+  return element;
+}
+
+data.forEach((pokemon) => {
+  cards.appendChild(createPokemonElement(pokemon));
+});
